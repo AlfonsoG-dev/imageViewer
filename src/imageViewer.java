cfb137ba7e069a238c8e301816e4214888c11a37
tuNovio <alfgomachi@gmail.com>
2024-01-28 18:31:39 -0500

    refactor: set the frame size

--- src/imageViewer.java
+++ src/imageViewer.java
@@ -2,6 +2,6 @@ import Interface.Panels.PanelPrincipal;
 
 class imageViewer {
     public static void main(String[] args) {
-        new PanelPrincipal();
+        new PanelPrincipal(1000, 900);
     }
 }
