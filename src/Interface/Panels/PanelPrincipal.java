5c646766a4f723a12942b3fa34d6370ac21e4ea6
tuNovio <alfgomachi@gmail.com>
2024-01-28 18:31:25 -0500

    refactor: cannot implement cut image with mouse

--- src/Interface/Panels/PanelPrincipal.java
+++ src/Interface/Panels/PanelPrincipal.java
@@ -20,14 +20,14 @@ public class PanelPrincipal {
     public PanelPrincipal(int width, int height) {
         createUI(width, height);
     }
-    public ImageIcon loadImage(int width, int height) {
+    public ImageIcon loadImage() {
         myImage = null;
         try {
             String imagePath = ".\\docs\\astronaut-guardian.jpg";
             ImageIcon toIcon = new ImageIcon(imagePath);
             Image original = toIcon.getImage();
-            int imageWidth = width;
-            int imageHeight = height;
+            int imageWidth = myFrame.getWidth()-200;
+            int imageHeight = myFrame.getHeight()-100;
             Image newSize = original.getScaledInstance(imageWidth, imageHeight, Image.SCALE_SMOOTH);
             myImage = new ImageIcon(newSize);
         } catch (Exception e) {
@@ -35,14 +35,14 @@ public class PanelPrincipal {
         }
         return myImage;
     }
-    public JPanel setPrincipalContent(int width, int height) {
+    public JPanel setPrincipalContent() {
         pPrincipal = new JPanel();
         pPrincipal.setLayout(new FlowLayout());
         
-        if(loadImage(width, height) == null) {
+        if(loadImage() == null) {
             imageLabel = new JLabel("cannot load image");
         } else {
-            imageLabel = new JLabel(loadImage(width, height));
+            imageLabel = new JLabel(loadImage());
         }
 
         pPrincipal.add(imageLabel);
@@ -73,9 +73,10 @@ public class PanelPrincipal {
         myFrame.setLayout(new BorderLayout());
         myFrame.setSize(width, height);
 
-        myFrame.add(setPrincipalContent(width - 200, height - 200), BorderLayout.CENTER);
+        myFrame.add(setPrincipalContent(), BorderLayout.CENTER);
         myFrame.add(setOptionsContent(), BorderLayout.SOUTH);
 
+        myFrame.pack();
         myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         myFrame.setVisible(true);
     }
