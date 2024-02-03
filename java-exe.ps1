a49567ff6398d702149469ea1a5b060bccaac715
AlfonsoG-dev <alfgomachi@gmail.com>
2024-02-03 10:47:55 -0500

    refactor: add Mundo to the compile clases

--- java-exe.ps1
+++ java-exe.ps1
@@ -1,4 +1,4 @@
-$compile = "javac -d .\bin\ .\src\*.java "
+$compile = "javac -Xlint:all -d .\bin\ .\src\*.java .\src\Interface\Panels\*.java .\src\Mundo\*.java "
 $createJar = "jar -cfm imageViewer.jar Manifesto.txt -C .\bin\ ."
 $javaCommand = "java -jar imageViewer.jar"
 $runCommand = "$compile" + " && " + "$createJar" + " && " +"$javaCommand"
