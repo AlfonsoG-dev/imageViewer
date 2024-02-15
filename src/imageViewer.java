ee40083896b8744bb8baf4753838c826fc1dc188
AlfonsoG-dev <alfgomachi@gmail.com>
2024-02-14 22:01:40 -0500

    refactor: add license terms

--- src/imageViewer.java
+++ src/imageViewer.java
@@ -1,3 +1,10 @@
+/**
+ * Copyright (C) 2024  Alfonso Gomajoa Achicanoy
+ * This program is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ */
 import Interface.Panels.SelectionPanel;
 
 class imageViewer {
